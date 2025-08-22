06b62e4de0de0528ad5efe662469d4a3e9965f9c
chdhmbr <chdhmbr@gmail.com>
2025-08-23 00:16:12 +0530

    Update GestureManagerActivity.kt

--- app/src/main/java/com/julkali/glauncher/GestureManagerActivity.kt
+++ app/src/main/java/com/julkali/glauncher/GestureManagerActivity.kt
@@ -5,21 +5,27 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.fragment.app.FragmentActivity
+import com.julkali.glauncher.databinding.ActivityGestureManagerBinding
 import com.julkali.glauncher.fragments.GestureViewerFragment
 import com.julkali.glauncher.io.database.GestureDBHandler
-import kotlinx.android.synthetic.main.activity_gesture_manager.*
 
 class GestureManagerActivity : FragmentActivity() {
 
     private lateinit var dbHandler: GestureDBHandler
+    private lateinit var binding: ActivityGestureManagerBinding
 
     private var displayedId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gesture_manager)
+        binding = ActivityGestureManagerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         dbHandler = GestureDBHandler(this)
         loadSavedApps()
+
+        // Wire up delete button
+        binding.deleteLaunchEntryButton.setOnClickListener { deleteAppLaunchEntry(it) }
     }
 
     fun onNewButtonClicked(view: View) {
@@ -34,21 +40,22 @@ class GestureManagerActivity : FragmentActivity() {
             return
         }
         hideNoGestureMessage()
-        val appEntriesSpinner = findViewById<Spinner>(R.id.savedAppEntries)
-        val entries = savedAppEntries
-            .map {
-                object {
-                    val id = it.id
-                    val appName = it.appName
-                    val gesture = it.gesture
 
-                    override fun toString(): String {
-                        return appName
-                    }
+        val entries = savedAppEntries.map {
+            object {
+                val id = it.id
+                val appName = it.appName
+                val gesture = it.gesture
+
+                override fun toString(): String {
+                    return appName
                 }
             }
+        }
+
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, entries)
-        appEntriesSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        binding.savedAppEntries.adapter = adapter
+        binding.savedAppEntries.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 Toast.makeText(applicationContext, "Please select something!", Toast.LENGTH_SHORT).show()
             }
@@ -60,17 +67,14 @@ class GestureManagerActivity : FragmentActivity() {
                 id: Long
             ) {
                 val selected = entries[position]
-                val fragment: GestureViewerFragment =
-                    this@GestureManagerActivity.gestureViewerFragment as GestureViewerFragment
+                val fragment = supportFragmentManager.findFragmentById(R.id.gestureViewerFragment) as GestureViewerFragment
                 fragment.display(
                     selected.appName,
                     selected.gesture
-                    )
+                )
                 displayedId = selected.id
             }
-
         }
-        appEntriesSpinner.adapter = adapter
     }
 
     override fun onResume() {
@@ -79,25 +83,21 @@ class GestureManagerActivity : FragmentActivity() {
     }
 
     private fun displayNoGesturesMessage() {
-        val appEntriesSpinner = findViewById<Spinner>(R.id.savedAppEntries)
-        val deleteButton = findViewById<Button>(R.id.deleteLaunchEntryButton)
-        appEntriesSpinner.visibility = View.GONE
-        deleteButton.visibility = View.GONE
-        val fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction().hide(gestureViewerFragment).commit()
-        val txtNoGestures = findViewById<TextView>(R.id.txtNoGestures)
-        txtNoGestures?.visibility = View.VISIBLE
+        binding.savedAppEntries.visibility = View.GONE
+        binding.deleteLaunchEntryButton.visibility = View.GONE
+        supportFragmentManager.beginTransaction()
+            .hide(supportFragmentManager.findFragmentById(R.id.gestureViewerFragment)!!)
+            .commit()
+        binding.txtNoGestures.visibility = View.VISIBLE
     }
 
     private fun hideNoGestureMessage() {
-        val appEntriesSpinner = findViewById<Spinner>(R.id.savedAppEntries)
-        val deleteButton = findViewById<Button>(R.id.deleteLaunchEntryButton)
-        appEntriesSpinner.visibility = View.VISIBLE
-        deleteButton.visibility = View.VISIBLE
-        val fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction().show(gestureViewerFragment).commit()
-        val txtNoGestures = findViewById<TextView>(R.id.txtNoGestures)
-        txtNoGestures?.visibility = View.GONE
+        binding.savedAppEntries.visibility = View.VISIBLE
+        binding.deleteLaunchEntryButton.visibility = View.VISIBLE
+        supportFragmentManager.beginTransaction()
+            .show(supportFragmentManager.findFragmentById(R.id.gestureViewerFragment)!!)
+            .commit()
+        binding.txtNoGestures.visibility = View.GONE
     }
 
     fun deleteAppLaunchEntry(view: View) {
